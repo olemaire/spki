199a979a7d5a3096a91f398b7eb4318b4a018321
Olivier Lemaire <olivier.lemaire@senso-rezo.com>
2014-02-12 04:40:16 +0100

    ...

---
+++ rust/spki.rs
@@ -0,0 +1,14 @@
+/*
+ * SPKI - Simple Public Key Infrastructure
+ *
+ *  Copyright © 2014 Senso-Rezo
+ *  All rigths reserved.
+ *
+ * See LICENSE file for licensing information.
+ * See http://github/olemaire/spki for more information.
+ */
+
+fn main() {
+}
+
+/* This Is The End */
